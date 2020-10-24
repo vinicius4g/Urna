afa0cce2f02645e133605f92ecaf2081610d5e0d
vinicius4g <viniciusdias1701@hotmail.com>
2020-10-23 23:48:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,142 @@
+//variaveis usadas para controle de interface
+let seuVotoPara = document.querySelector('.textoInicial span');
+let cargo = document.querySelector('.cargo span');
+let descricao = document.querySelector('.infoRestante');
+let aviso = document.querySelector('.divisao2');
+let lateral = document.querySelector('.divisao1-right');
+let numeros = document.querySelector('.numeroDigitado');
+
+//variaveis usadas para controle de ambiente (saber qual etapa esta)
+let etapaAtual = 0;
+let numero = '';
+let votoBranco = false;
+let votos = [];
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
+
+    for (let i=0; i<etapa.numeros; i++){
+        if(i === 0 ){
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>'
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if (item.numero === numero) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+    
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            if(candidato.fotos[i].url.small){
+                fotosHtml += `<div class="imagemUrna small"> <img src="assets/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            }
+            else{
+                fotosHtml += `<div class="imagemUrna"> <img src="assets/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            }
+            
+        }
+
+        lateral.innerHTML = fotosHtml;
+    }
+    else{
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
+}
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if (elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca');
+        }
+        else {
+            atualizaInterface();
+        }
+    }
+}
+
+function branco() {
+    if(numero === '') {
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    }
+    else {
+        alert("Para votar em BRANCO, não pode ter digitado nenhum número");
+    }
+}
+
+function corrige() {
+    comecarEtapa();
+}
+
+function confirma() {
+            
+    let etapa = etapas[etapaAtual];
+
+    let votoConfirmado = false;
+    
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+    }
+    else if (numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });  
+    }
+
+    if(votoConfirmado) {
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined) {
+            comecarEtapa();
+        }
+        else {
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+            console.log(votos);
+        }
+    }
+}
+
+comecarEtapa();
